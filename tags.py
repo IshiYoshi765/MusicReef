8243e1ef51ef4e0280da10f968047db168c509f7
takumi <t.souma.sys22@morijyobi.ac.jp>
2023-10-31 10:34:03 +0900

    タグ追加機能no

---
+++ tags.py
@@ -0,0 +1,18 @@
+import os
+import psycopg2 
+
+def get_connection():
+    url = os.environ['DATABASE_URL']
+    connection = psycopg2.connect(url)
+    return connection
+
+def insert_tags(genre,tag_name):
+    connection = get_connection()
+    cursor = connection.cursor()
+    sql = 'INSERT INTO tags VALUES (default,%s,%s)'
+    
+    cursor.execute(sql,(genre,tag_name))
+    
+    connection.commit()
+    cursor.close()
+    connection.close() 
