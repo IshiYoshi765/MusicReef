65a1b8b8e414718ade188ecb1ff8f1d9a98a76eb
よっし <yo.ishikawa.sys22@morijyobi.ac.jp>
2023-12-14 09:34:18 +0900

    口コミ一覧（ここから削除ボタンを付ける）

--- static/js/script.js
+++ static/js/script.js
@@ -59,19 +59,44 @@ function closeModal() {
 }
 //ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 
+// 口コミのモーダルーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+document.addEventListener("DOMContentLoaded", function () {
+  // 何らかの初期化処理
+});
 
-// 口コミのモーダル
-const reviewModal = document.querySelector(".review-modal");
+// 口コミ一覧を取得して表示する関数
+function fetchAndOpenReviewModal(musicId) {
+  // ここでサーバーサイドのAPIを呼び出して音源情報と口コミデータを取得
+  fetch(`/api/musicinfo?music_id=${musicId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // 取得したデータを表示
+      displayMusicInfoAndReviews(data);
+      // モーダルを表示
+      openReviewModal();
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+}
 
-const reviewModalButtons = document.querySelectorAll(".js-review-modal-button");
-reviewModalButtons.forEach((elm, i) => {
-  elm.addEventListener("click", (e) => {
-    reviewModal.classList.add("is-open");
+// 取得したデータを表示する関数
+function displayMusicInfoAndReviews(data) {
+  let modalBody = document.getElementById("reviewList");
+  modalBody.innerHTML = "";
+
+  // 口コミ一覧を表示
+  data.reviews.forEach(function (review) {
+    let reviewItem = document.createElement("div");
+    reviewItem.innerHTML = `<div style="display: flex;">
+    <p style="margin-left: 10px;">★: ${review[3]}</p>
+    <p style="margin-left: 40px;">口コミ: ${review[4]}</p>
+  </div>`;
+    modalBody.appendChild(reviewItem);
   });
-});
-//モーダルの閉じるボタン
-const modalClose2 = document.querySelector(".js-review-close-button");
-modalClose2.addEventListener("click", () => {
-  reviewModal.classList.remove("is-open");
-});
+  document.querySelector(".layer.review-modal").classList.add("is-open");
+}
 
+function closeReviewModal() {
+  document.querySelector(".layer.review-modal").classList.remove("is-open");
+}
+//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
