ca27585f6efd6df2d3f7afc2c4603098a7eaaa79
takumi <t.souma.sys22@morijyobi.ac.jp>
2023-12-19 11:24:58 +0900

    sessionの時間を延ばした

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def login():
             else:
                 session["user"] = mail  # session にキー：'user', バリュー:True を追加
                 session.permanent = True  # session の有効期限を有効化
-                app.permanent_session_lifetime = timedelta(minutes=5)  # session の有効期限を 5 分に設定
+                app.permanent_session_lifetime = timedelta(minutes=30)  # session の有効期限を 5 分に設定
                 return redirect(url_for("mypage"))
         else:
            
